3c0ddb4d70f55a3face113f482e9dfbfaf6afb20
ManuPepermans <manu.pepermans@student.uantwerp.be>
2018-04-19 23:24:26 +0200

    JSON communication set-up

--- main.cpp
+++ main.cpp
@@ -7,25 +7,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <json.hpp>
+#include <sstream>
+#include <string>
+
+
 
 using namespace std;
+using json = nlohmann::json;
+
+
+string str;
+json clientInfo;
+json accepted;
+int client;
+int portNum = 6666; // NOTE that the port number is same for both client and server
+bool isExit = false;
+const int bufsize = 1024;
+char buffer[bufsize];
+char* ip = "127.0.0.1";
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
 
-    int client;
-    int portNum = 6666; // NOTE that the port number is same for both client and server
-    bool isExit = false;
-    int bufsize = 1024;
-    char buffer[bufsize];
-    char* ip = "127.0.0.1";
+
+std::cout << "Hello, World!" << std::endl;
+
 
     struct sockaddr_in server_addr;
+    server_addr.sin_port = htons(portNum);
+    inet_pton(AF_INET, ip, &server_addr.sin_addr);
+
 
     client = socket(AF_INET, SOCK_STREAM, 0);
-
-    /* ---------- ESTABLISHING SOCKET CONNECTION ----------*/
-    /* --------------- socket() function ------------------*/
 
     if (client < 0)
     {
@@ -33,96 +46,41 @@ int main() {
         exit(1);
     }
 
-    /*
-        The socket() function creates a new socket.
-        It takes 3 arguments,
-            a. AF_INET: address domain of the socket.
-            b. SOCK_STREAM: Type of socket. a stream socket in
-            which characters are read in a continuous stream (TCP)
-            c. Third is a protocol argument: should always be 0. The
-            OS will choose the most appropiate protocol.
-            This will return a small integer and is used for all
-            references to this socket. If the socket call fails,
-            it returns -1.
-    */
-
-    cout << "\n=> Socket client has been created..." << endl;
-
-    /*
-        The variable serv_addr is a structure of sockaddr_in.
-        sin_family contains a code for the address family.
-        It should always be set to AF_INET.
-        htons() converts the port number from host byte order
-        to a port number in network byte order.
-    */
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(portNum);
-
-    // this function returns returns 1 if the IP is valid
-    // and 0 if invalid
-    // inet_pton converts IP to packets
-    // inet_ntoa converts back packets to IP
-    //inet_pton(AF_INET, ip, &server_addr.sin_addr);
-
-    /*if (connect(client,(struct sockaddr *)&server_addr, sizeof(server_addr)) == 0)
-        cout << "=> Connection to the server " << inet_ntoa(server_addr.sin_addr) << " with port number: " << portNum << endl;*/
-
-
-    /* ---------- CONNECTING THE SOCKET ---------- */
-    /* ---------------- connect() ---------------- */
-
-
     if (connect(client,(struct sockaddr *)&server_addr, sizeof(server_addr)) == 0)
     {
-        char port[]=  "9090\n";
-        char carType[] = "true\n";
-        send(client, port,strlen(port),0);
+        cout << "=> Connection to the server " << inet_ntoa(server_addr.sin_addr) << " with port number: "  << portNum << endl;
+        char port[]= "9090";
+        char carType[] = "true";
+        char model[] = "F1";
+        clientInfo["port"] = port;
+        clientInfo["robot"] = carType;
+        clientInfo["model"] = model;
+
+        std::string s =  clientInfo.dump();
+        const char *test1 = s.c_str();
+
+        send(client,test1,strlen(test1),0);
+        send(client,"\n",strlen("\n"),0);
+
         send(client, carType,strlen(carType),0);
-        cout << "=> Connection confirmed, you are good to go...";
+        recv(client, buffer, bufsize - 1, 0);
+        std::string str (buffer);
+        auto j3 = json::parse(str);
+
+
+        std::cout << j3.at("id") << '\n';
+        do {
+            sleep(5000);
+        } while (isExit);
 
     }
+    else
+        cout << "could nog connect" << endl;
 
 
-    // Once it reaches here, the client can send a message first.
 
-    do {
-        cout << "Client: ";
-        do {
-            cin >> buffer;
-            send(client, buffer, bufsize, 0);
-            if (*buffer == '#') {
-                send(client, buffer, bufsize, 0);
-                *buffer = '*';
-                isExit = true;
-            }
-        } while (*buffer != 42);
-
-        cout << "Server: ";
-        do {
-            recv(client, buffer, bufsize, 0);
-            cout << buffer << " ";
-            if (*buffer == '#') {
-                *buffer = '*';
-                isExit = true;
-            }
-
-        } while (*buffer != 42);
-        cout << endl;
-
-    } while (!isExit);
-
-    /* ---------------- CLOSE CALL ------------- */
-    /* ----------------- close() --------------- */
-
-    /*
-        Once the server presses # to end the connection,
-        the loop will break and it will close the server
-        socket connection and the client connection.
-    */
 
     cout << "\n=> Connection terminated.\nGoodbye...\n";
-
     close(client);
     return 0;
 }
